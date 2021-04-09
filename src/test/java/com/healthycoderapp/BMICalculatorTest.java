cc24400e692934693572362c9549418090342d86
linus <linus.pauls@telebasel.ch>
2021-04-09 14:36:13 +0200

    'init'

---
+++ src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -0,0 +1,105 @@
+package com.healthycoderapp;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BMICalculatorTest {
+
+    @ParameterizedTest
+    @ValueSource(doubles = {90.0, 99.9, 103, 11000})
+    void shouldReturnTrue_when_DietIsRecommended(Double coderWeight) {
+        //given
+        double weight = coderWeight;
+        double height = 1.70;
+
+        //when
+        boolean recommendation = BMICalculator.isDietRecommended(weight, height);
+
+        //then
+        assertTrue(recommendation);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"70,1.70", "59, 1.65", "85, 1.85"})
+    void should_return_false_when_Diet_is_not_Recommended(Double coderWeight, Double coderHeight) {
+        //given
+        double weight = coderWeight;
+        double height = coderHeight;
+
+        //when
+        boolean recommendation = BMICalculator.isDietRecommended(weight, height);
+
+        //then
+        assertFalse(recommendation);
+    }
+
+    @Test
+    void should_throw_ArithmeticException_when_HeightZero() {
+        //given
+        double weight = 70;
+        double height = 0;
+
+        //when
+        Executable executable = () -> BMICalculator.isDietRecommended(weight, height);
+
+        //then
+        assertThrows(ArithmeticException.class, executable);
+    }
+
+    @Test
+    void should_returnCoderWithWorstBMI_whenCoderListNotEmpty(){
+        //given
+        List<Coder> coderList = new ArrayList<>();
+        coderList.add(new Coder(1.80, 90, 32, Gender.MALE));
+        coderList.add(new Coder(1.65, 58, 28, Gender.FEMALE));
+        coderList.add(new Coder(1.72, 99, 33, Gender.MALE));
+
+        //when
+        Coder coderWorstBMI = BMICalculator.findCoderWithWorstBMI(coderList);
+
+        //then
+        assertAll(
+                () -> assertEquals(99, coderWorstBMI.getWeight()),
+                () -> assertEquals(1.72, coderWorstBMI.getHeight())
+        );
+    }
+
+    @Test
+    void should_returnNull_whenCoderListEmpty(){
+        //given
+        List<Coder> coderList = new ArrayList<>();
+
+        //when
+        Coder coderWorstBMI = BMICalculator.findCoderWithWorstBMI(coderList);
+
+        //then
+        assertNull(coderWorstBMI);
+    }
+
+    @Test
+    void should_returnCorrectBmiArray_whenCoderListNotEmpty(){
+
+        //given
+        List<Coder> coderList = new ArrayList<>();
+        coderList.add(new Coder(1.80, 90, 32, Gender.MALE));
+        coderList.add(new Coder(1.65, 58, 28, Gender.FEMALE));
+        coderList.add(new Coder(1.72, 99, 33, Gender.MALE));
+
+        double[] expectedValues = {27.78, 21.30, 33.46};
+
+        //when
+        double[] coderBMIs = BMICalculator.getBMIScores(coderList);
+
+        //then
+        assertArrayEquals(expectedValues, coderBMIs);
+
+    }
+}
---
+++ src/test/java/com/healthycoderapp/DietPlannerTest.java
@@ -0,0 +1,48 @@
+package com.healthycoderapp;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DietPlannerTest {
+
+    private DietPlanner dietPlanner;
+
+    @BeforeAll
+    static void beforeAll(){//bevorAll must be static
+        System.out.println("starting tests");
+    }
+
+    @AfterAll
+    static void afterAll(){//bevorAll must be static
+        System.out.println("done testing, closing all connections");
+    }
+
+    @BeforeEach
+    void setUp() {
+        this.dietPlanner = new DietPlanner(20, 30, 50);
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("a unittest has finished");
+    }
+
+    @Test
+    void shouldReturnCorrectDietPlan_when_givenCorrectCoder(){
+        //given
+        Coder coder = new Coder(1.82, 75.0, 26, Gender.MALE);
+        DietPlan expected = new DietPlan(2202, 110, 73, 275);
+
+        //when
+        DietPlan actual = dietPlanner.calculateDiet(coder);
+
+        //then
+        assertAll(
+                () -> assertEquals(actual.getCalories(), expected.getCalories()),
+                () -> assertEquals(actual.getCarbohydrate(), expected.getCarbohydrate()),
+                () -> assertEquals(actual.getFat(), expected.getFat()),
+                () -> assertEquals(actual.getProtein(), expected.getProtein())
+        );
+    }
+}
